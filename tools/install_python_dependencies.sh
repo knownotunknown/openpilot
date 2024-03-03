6c4af044958e9d3f1ae94adba0603de65c2318fd
knownotunknown <78577376+knownotunknown@users.noreply.github.com>
2024-03-03 14:11:55 -0600

    Hopefully fixed pyenv error

--- tools/install_python_dependencies.sh
+++ tools/install_python_dependencies.sh
@@ -5,61 +5,33 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 ROOT=$DIR/../
 cd $ROOT
 
-RC_FILE="${HOME}/.$(basename ${SHELL})rc"
-if [ "$(uname)" == "Darwin" ] && [ $SHELL == "/bin/bash" ]; then
-  RC_FILE="$HOME/.bash_profile"
-fi
-
 if ! command -v "pyenv" > /dev/null 2>&1; then
   echo "pyenv not found in PATH. Installing pyenv..."
-  if [ -d "$HOME/.pyenv" ]; then
-    echo "Found existing .pyenv directory. Skipping pyenv installation."
-  else
+  if [ ! -d "$HOME/.pyenv" ]; then
     curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
-    PYENV_PATH_SETUP="export PATH=\$HOME/.pyenv/bin:\$HOME/.pyenv/shims:\$PATH"
+  else
+    echo "Found existing .pyenv directory. Skipping pyenv installation."
   fi
+  # Directly export PATH changes to ensure they are applied in the current shell session
+  export PATH="$HOME/.pyenv/bin:$HOME/.pyenv/shims:$PATH"
 else
   echo "pyenv is already installed."
 fi
 
-if [ -z "$PYENV_SHELL" ] || [ -n "$PYENV_PATH_SETUP" ]; then
-  echo "pyenvrc setup ..."
-  cat <<EOF > "${HOME}/.pyenvrc"
-if [ -z "\$PYENV_ROOT" ]; then
-  $PYENV_PATH_SETUP
-  export PYENV_ROOT="\$HOME/.pyenv"
-  eval "\$(pyenv init -)"
-  eval "\$(pyenv virtualenv-init -)"
-fi
-EOF
-
-  SOURCE_PYENVRC="source ~/.pyenvrc"
-  if ! grep "^$SOURCE_PYENVRC$" $RC_FILE > /dev/null; then
-    printf "\n$SOURCE_PYENVRC\n" >> $RC_FILE
-  fi
-
-  eval "$SOURCE_PYENVRC"
-  # $(pyenv init -) produces a function which is broken on bash 3.2 which ships on macOS
-  if [ $(uname) == "Darwin" ]; then
-    unset -f pyenv
-  fi
-fi
+# Initialize pyenv in the current script
+export PYENV_ROOT="$HOME/.pyenv"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 
 export MAKEFLAGS="-j$(nproc)"
 
 PYENV_PYTHON_VERSION=$(cat $ROOT/.python-version)
 if ! pyenv prefix ${PYENV_PYTHON_VERSION} &> /dev/null; then
-  # no pyenv update on mac
-  if [ "$(uname)" == "Linux" ]; then
-    echo "pyenv update ..."
-    pyenv update
-  fi
   echo "python ${PYENV_PYTHON_VERSION} install ..."
   CONFIGURE_OPTS="--enable-shared" pyenv install -f ${PYENV_PYTHON_VERSION}
 fi
-eval "$(pyenv init --path)"
 
-echo "update pip"
 pip install pip==23.3
 
 poetry config virtualenvs.prefer-active-python true --local
